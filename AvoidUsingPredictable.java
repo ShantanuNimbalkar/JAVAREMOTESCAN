f38864da95c2678faa71665c2abf3580e0857807
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2022-08-29 13:33:41 +0530

    Update AvoidUsingPredictable.java

--- AvoidUsingPredictable.java
+++ AvoidUsingPredictable.java
@@ -12,4 +12,8 @@ public class AvoidUsingPredictable {
         // EMB-ISSUE: CodeIssueNames.AVOID_USING_PREDICTABLE_RANDOM_VALUES/no-detect
         SecureRandom secureRandom = new SecureRandom();
     }
+    
+    public void print(){
+    System.out.println("hello");
+    }
 }
