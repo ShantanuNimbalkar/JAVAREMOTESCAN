0c8f49c496f7c903b69a20be4d99e86d074ae16d
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2023-03-29 17:29:51 +0530

    Update ReturnInFinallyBlockNew.java

--- ReturnInFinallyBlockNew.java
+++ ReturnInFinallyBlockNew.java
@@ -93,7 +93,7 @@ public class ReturnInFinallyBlockNew {
         {
             if(true)
             {
-                //System.out.println("Hi");
+                System.out.println("Hi");
                 return;
 
             }
