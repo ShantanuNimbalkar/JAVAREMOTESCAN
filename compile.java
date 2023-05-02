3574d828a5b817243d73b985aededac63ca391b9
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2023-05-02 11:41:45 +0530

    Update compile.java

--- compile.java
+++ compile.java
@@ -2,6 +2,18 @@ public class compile{
 	static final SecureRandom secureRandom = new SecureRandom();
 
 	 int GenerateRandomValue(){
+	 System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("Hello");
         return secureRandom.nextInt();
     }
 }
