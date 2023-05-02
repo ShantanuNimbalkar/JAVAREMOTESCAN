7ba113f25ece75f350499e45c66f437c3683efc2
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2023-05-02 12:11:18 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -6,5 +6,20 @@ import java.util.Random;
 public class Demo {
     public void printmessage(){
         //System.out.println("Demo");
+        System.out.println("New Issue");
+    System.out.println("New Issue");
+    System.out.println("New Issue");
+    System.out.println("New Issue");
+    System.out.println("New Issue");
+    System.out.println("New Issue");
+    System.out.println("New Issue");
+    System.out.println("New Issue");
+    System.out.println("New Issue");
+    System.out.println("New Issue");
+    System.out.println("New Issue");
+    System.out.println("New Issue");
+    System.out.println("New Issue");
+    System.out.println("New Issue");
+    System.out.println("New Issue");
     }
 }
