02a7c189b129199b594dfd9ead1e74edf8709002
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2023-10-12 12:47:23 +0530

    TEM-1: added logs

--- Demo.java
+++ Demo.java
@@ -28,6 +28,11 @@ public class Demo {
                 System.out.println("Hi");
                 System.out.println("Hi");
                 System.out.println("Hi");
+                System.out.println("Hello");
+                System.out.println("Hello");
+                System.out.println("Hello");
+                System.out.println("Hello");
+                System.out.println("Hello");
                 return;
             }
         }
